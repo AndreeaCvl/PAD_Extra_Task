69266703a0c896016d1825ef32058c69b21e42bd
AndreeaCvl <44116789+AndreeaCvl@users.noreply.github.com>
2024-01-17 17:09:58 +0200

    add both databases with docker

--- matches_ms/matches_ms.py
+++ matches_ms/matches_ms.py
@@ -1,6 +1,6 @@
 import json
-
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
 import http.client
 from datetime import datetime
 
@@ -10,6 +10,44 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello World"
+
+@app.route('/pingdb')
+def ping_db():
+    db_params = {
+        'host': 'matches-db.pad',
+        'database': 'matches_db',
+        'user': 'admin',
+        'password': 'mysecretpassword',
+        'port': '5432'
+    }
+
+    # Establish a connection to the PostgreSQL database
+    try:
+        connection = psycopg2.connect(**db_params)
+        cursor = connection.cursor()
+
+        # Execute a query to get the current timestamp from the database
+        cursor.execute("SELECT current_timestamp;")
+
+        # Fetch the result
+        current_time = cursor.fetchone()[0]
+
+        # Print the current time
+        print(f"Current time from the database: {current_time}")
+
+        # Close the cursor and connection
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Connection closed.")
+
+        return jsonify(current_time), 200
+
+    except psycopg2.Error as e:
+        print(f"Error connecting to the database: {e}")
+        return str(e), 500
+
+
 
 @app.route('/update')
 def update_db():
--- weather_ms/weather_ms.py
+++ weather_ms/weather_ms.py
@@ -1,5 +1,9 @@
 import requests
-from flask import Flask
+import json
+from flask import Flask, jsonify
+import psycopg2
+import http.client
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -15,6 +19,44 @@ headers = {
 response = requests.get(url, headers=headers, params=querystring)
 
 print(response.json())
+
+
+
+@app.route('/pingdb')
+def ping_db():
+    db_params = {
+        'host': 'weather-db.pad',
+        'database': 'weather_db',
+        'user': 'admin',
+        'password': 'mysecretpassword',
+        'port': '5432'
+    }
+
+    # Establish a connection to the PostgreSQL database
+    try:
+        connection = psycopg2.connect(**db_params)
+        cursor = connection.cursor()
+
+        # Execute a query to get the current timestamp from the database
+        cursor.execute("SELECT current_timestamp;")
+
+        # Fetch the result
+        current_time = cursor.fetchone()[0]
+
+        # Print the current time
+        print(f"Current time from the database: {current_time}")
+
+        # Close the cursor and connection
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Connection closed.")
+
+        return jsonify(current_time), 200
+
+    except psycopg2.Error as e:
+        print(f"Error connecting to the database: {e}")
+        return str(e), 500
 
 
 @app.route('/')
